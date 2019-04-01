331e185d2ecd33149025e829f37ae24486ecc672
Tom Dunn <thomas.dunn@gmail.com>
2019-03-31 20:23:21 -0400

    Update github token and redirect errors.

--- .travis/push.sh
+++ .travis/push.sh
@@ -7,5 +7,5 @@ git diff-index --quiet HEAD -- && exit 0
 echo "There are changes to be committed"
 git commit -m "Pattern update from Travis CI - build #$TRAVIS_BUILD_NUMBER - $(date -Iseconds)"
 npm version patch -m "Pattern update version bump to %s"
-git remote set-url origin https://${GITHUB_TOKEN}@github.com/thomasdunn/cellular-automata-patterns.git
-git push --quiet --follow-tags --set-upstream origin master
+git remote set-url origin https://${GITHUB_TOKEN}@github.com/thomasdunn/cellular-automata-patterns.git > /dev/null 2>&1
+git push --quiet --follow-tags --set-upstream origin master > /dev/null 2>&1
